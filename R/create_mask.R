6904d071841467e0e3f77c644a53411540454b67
bshafer8 <bshafe8@gmail.com>
2020-04-20 11:09:12 -0400

    mask template

--- R/create_mask.R
+++ R/create_mask.R
@@ -7,8 +7,14 @@ plant_stack <- brick(plant_ras)
 harv_ras <- lapply(harv, raster)
 harv_stack <- brick(harv_ras)
 
-mask_proj <- projectRaster(from = maskr, to = plant_stack)
+mask_proj <- projectRaster(from = maskr, to = harv_masked)
 crop_mask <- reclassify(mask_proj, c(.75, 1, 1, -1, .7499, NA))
 
 plant_masked <- mask(x = plant_stack, mask = crop_mask)
 harv_masked <- mask(x = harv_stack, mask = crop_mask)
+cellStats(plant_masked, stat = 'mean', na.rm = TRUE)
+
+plant_mean
+negative_ras <- (plant_mean > 365) * -365
+plant_mean_corrected <- plant_mean + negative_ras
+plant_mean_corrected
